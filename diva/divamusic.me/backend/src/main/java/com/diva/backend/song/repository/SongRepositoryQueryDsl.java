10592d75e1c7cfe981ec41252ef114657870a1ba
gnaak <o101srb@daum.net>
2024-02-25 23:19:18 +0900

    diva

--- diva/divamusic.me/backend/src/main/java/com/diva/backend/song/repository/SongRepositoryQueryDsl.java
+++
@@ -1,9 +0,0 @@
-package com.diva.backend.song.repository;
-
-import com.diva.backend.recommend.service.dto.RecommendedSongsResponseDto;
-
-import java.util.List;
-
-public interface SongRepositoryQueryDsl {
-    List<RecommendedSongsResponseDto> getTop3SimilarSongs(int membersMaxMidi);
-}
